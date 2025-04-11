5477fb51f2b2b8010a376048885308ef2f9f9140
ci-sumi <tharayilsurendransumi@gmail.com>
2025-04-11 10:52:54 +0100

    create book model

--- library_management/books/models.py
+++ library_management/books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    book_id = models.CharField(max_length=20,unique=True)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    subject = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.title
+    

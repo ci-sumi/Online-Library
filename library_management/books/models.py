b9f75f0e0ce84834fdd8f76a52a336d05db27906
ci-sumi <tharayilsurendransumi@gmail.com>
2025-04-11 11:07:21 +0100

    Update book model to include pubished date

--- library_management/books/models.py
+++ library_management/books/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Books(models.Model):
+class Book(models.Model):
     book_id = models.CharField(max_length=20,unique=True)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     subject = models.CharField(max_length=200)
+    published_date = models.DateField()
     
     def __str__(self):
         return self.title

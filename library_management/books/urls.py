a723558d1e41e2301c6658af0bc1878e190ef1b3
ci-sumi <tharayilsurendransumi@gmail.com>
2025-04-28 19:13:41 +0100

    Add URL patterns for index page and all books page

--- library_management/books/urls.py
+++ library_management/books/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('', views.index, name='index'),
     path('books/',views.index,name='index'),
+    path('books/all/',views.all_book_view,name='all_book_view')
 
 ]

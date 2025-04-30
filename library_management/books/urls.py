657ec87f045f26ec24dc0bd93649cb195c746c29
ci-sumi <tharayilsurendransumi@gmail.com>
2025-04-30 11:07:46 +0100

    Map url pattern for save_to_database_view

--- library_management/books/urls.py
+++ library_management/books/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/',views.index,name='index'),
-    path('books/all/',views.all_book_view,name='all_book_view')
+    path('books/all/',views.all_book_view,name='all_book_view'),
+    path('save-books/',views.save_to_database,name='save_to_database'),
 
 ]
